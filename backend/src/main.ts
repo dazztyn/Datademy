e9747b8f0147fa5ce0d32dd1bffb57b65b5914b5
Pablo12331 <pablo.jorquera@alumnos.ucn.cl>
2026-05-05 18:59:24 -0400

    a

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Borra cualquier dato extra que un hacker intente enviar
-    forbidNonWhitelisted: true, // Lanza un error si envían datos no permitidos
+    whitelist: true,
+    forbidNonWhitelisted: true, 
   }));
   app.enableCors({
   origin: 'http://localhost:5173',
