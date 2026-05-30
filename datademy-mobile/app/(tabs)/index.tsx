4e440c7faa320f638aa919159c818decf915148e
Vicente Ruiz Escobar <greatdazz3@gmail.com>
2026-05-29 20:39:28 -0400

    mem

--- datademy-mobile/app/(tabs)/index.tsx
+++ datademy-mobile/app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ export default function HomeScreen() {
       
       {/* Reemplazamos el <h1> por <Text> */}
       <Text className="text-3xl font-bold text-blue-400 mb-2">
-        ¡NativeWind Funciona! 🎉
+        ¡NativeWind Funciona!
       </Text>
       
       {/* Reemplazamos el <p> por <Text> */}
