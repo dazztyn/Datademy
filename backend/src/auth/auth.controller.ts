bd108504a6511f19a8abd60e2076cceee31acdfe
Pablo12331 <pablo.jorquera@alumnos.ucn.cl>
2026-06-02 22:19:41 -0400

    a

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -6,15 +6,15 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  //El botón del frontend apunta aquí. Esto redirige a la pantalla de Google.
   @Get('google')
   @UseGuards(AuthGuard('google'))
   async inicioSesionGoogle() {}
 
-  // A donde Google nos devuelve después de que el usuario acepta los permisos
+
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  async googleAuthRedirect(@Req() req, @Res() res) {
+  async googleAuthRedirect(@Req() req, @Res() res) 
+  {
     const resultadoLogin = await this.authService.validarUsuarioGoogle(req.user);
 
     const urlFrontend = 'http://localhost:5173/login';
@@ -23,13 +23,6 @@ export class AuthController {
     const gToken = resultadoLogin.tokens.googleAccessToken;
     
     console.log(jwt);
-
-    //Despues se debe cambiar descomentar
     return res.redirect(`${urlFrontend}?token=${jwt}&gToken=${gToken}`);
-
-    /*return res.json({
-      mensaje: "Copia este token JWT para usarlo en Thunder Client",
-      token_jwt: jwt
-    });*/
   }
 }
