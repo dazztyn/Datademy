8b54174cfd8991ac696c8844ddd07b071dc5fd47
Pablo12331 <pablo.jorquera@alumnos.ucn.cl>
2026-06-10 11:31:09 -0400

    Quitar limite

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const frontendUrl = configService.get<string>('FRONTEND_URL') || 'http://localhost:5173';
+
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
   
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
