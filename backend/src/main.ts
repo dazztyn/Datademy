af553ace81248e8b9f41b9e4acae6bb9fa6d6579
Pablo12331 <pablo.jorquera@alumnos.ucn.cl>
2026-06-29 18:42:27 -0400

    funciona porfa :(

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,13 @@ async function bootstrap()
 
   app.getHttpAdapter().getInstance().set('trust proxy', 1);
 
-  app.use(helmet());
+  app.use(helmet(
+    {
+      contentSecurityPolicy: false,
+      crossOriginEmbedderPolicy: false,
+      crossOriginOpenerPolicy: false,
+    }
+  ));
 
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ limit: '10mb', extended: true }));
