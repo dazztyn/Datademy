b577c8d175598210796d55b4b701143279b5fae6
Pablo12331 <pablo.jorquera@alumnos.ucn.cl>
2026-06-29 18:03:35 -0400

    NOSQL ME LO VOY A HECHAR

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -44,7 +44,7 @@ import { join } from 'path';
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'), 
-      exclude: ['/api/:path*'],
+      exclude: ['/api/{*splat}'],
     }),
   ],
   controllers: 
