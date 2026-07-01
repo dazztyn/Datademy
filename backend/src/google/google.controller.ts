a11e32daa1819750c05af2dfe547491cbbf262ca
Pablo12331 <pablo.jorquera@alumnos.ucn.cl>
2026-07-01 02:29:59 -0400

    a

--- backend/src/google/google.controller.ts
+++ backend/src/google/google.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, Param, Post, Body } from '@nestjs/common';
-import { GoogleService } from './google.service';
 
 @Controller('google')
 export class GoogleController {
-  constructor(private readonly googleService: GoogleService) {}
+  constructor() {}
 
 }
