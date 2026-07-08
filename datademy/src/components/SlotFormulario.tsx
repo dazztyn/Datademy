e41dd00d1a0f2fa70b0c14432c0aca1a4bd8d16a
IenZAVALA-S <166187325+IenZAVALA-S@users.noreply.github.com>
2026-07-07 20:09:03 -0400

    mini mini mini fix

--- datademy/src/components/SlotFormulario.tsx
+++ datademy/src/components/SlotFormulario.tsx
@@ -126,13 +126,13 @@ export default function SlotFormulario({ label, asignado, idGoogleForm, idProces
                         onClick={() => { setMenuAbierto(false); setMostrarDesasignar(true)}}
                         className="w-full text-left px-3 py-2 text-md text-red-400 hover:bg-red-50 dark:hover:bg-red-900/20 transition-colors"
                       >
-                        ✕ Desasignar
+                        ☼ Desasignar
                       </button>
                       <button
                       onClick={() => { setMenuAbierto(false); setMostrarEliminar(true) }}
                       className="w-full text-left px-3 py-2 text-md text-red-600 dark:text-red-500 font-medium hover:bg-red-50 dark:hover:bg-red-900/20 transition-colors"
                     >
-                      🗑 Eliminar formulario
+                      ✕ Eliminar formulario
                     </button>
                     </div>
                   </>,
